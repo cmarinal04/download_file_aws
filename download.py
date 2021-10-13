cc2c0c74344170b3879b3a15b9530159749b85e2
Johnatan Lopez Patino <Johnatan.Lopez@tigo.com.co>
2021-10-13 11:38:09 -0500

    Se actualiza gitignore

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
-
+import pandas as pd
+from connection import sql_server_connection as con_sql
 from boto3.session import Session
 import boto3
 from _class.config import config_session
@@ -20,10 +21,15 @@ def download_file():
     date_1 = date.strftime(yesterday, '%Y%m%d')
     year = date.strftime(yesterday, '%Y')
     month = date.strftime(yesterday, '%m')
-    file_name = f'MTT_Pagos{date_1}.csv' #Acá, agregar ruta donde descargar los archivos.
-
+    file_name = (f'MTT_Pagos{date_1}.csv') #Acá, agregar ruta donde descargar los archivos.
     your_bucket = s3.Bucket(f'paymentgateway-billingsystem-prod')
-    your_bucket.download_file(f'CO/outbox/{year}/{month}/{file_name}',file_name)
+    file_local = f'./Soporte/{file_name}'
+    your_bucket.download_file(f'CO/outbox/{year}/{month}/{file_name}', file_local)
+
+    load(file_local)
+
+
+    
     # /CO/outbox/{year}/{month}/
     # for s3_file in your_bucket.objects.all():
     #     print(s3_file.key) # prints the contents of bucket
@@ -34,14 +40,33 @@ def download_file():
 
 
 
-def read():
-    pass
+def read(filename):
+    df = pd.read_csv(f'{filename}', dtype=str)
+    dict_columns = {
+        'Application/Billing System Name': 'Application Billing System Name',
+        'Nombre Columna en Pandas': 'Nombre en la tabla sql'
+    }
+    df.rename(columns={'Application/Billing System Name': 'Application Billing System Name'}, inplace=True)
+    df.fillna('', inplace=True)
+    # breakpoint()
+    return df #.iloc[:10]
 
 
-def load():
-    pass
+def load(filename):
+    df_read = read(filename)
+    records = df_read.values.tolist()
+    columns = [f'[{col}]' for col in df_read.columns.tolist()]
+    sql_insert = f'''INSERT INTO [dbo].[MTT_Pagos_Prueba_Test] ({', '.join(columns)})
+              VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,
+                       ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,
+                       ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+    '''
 
-
+   
+    connection = con_sql.connection()
+    cursor = connection.cursor()
+    cursor.executemany(sql_insert,records)
+    connection.commit()
 
 def main():
     download_file()
